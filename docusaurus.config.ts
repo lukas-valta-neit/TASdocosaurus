2ec920f544a87fb0c233615a5058c62d33a7893a
Lukas Valta <lukas.valta@neit.group>
2024-06-05 11:19:56 +0200

    loc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,10 +24,10 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "cs",
-    locales: ["cs"],
-  },
+  // i18n: {
+  //   defaultLocale: "cs",
+  //   locales: ["cs"],
+  // },
 
   presets: [
     [
